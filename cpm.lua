12ee66170908d53f01d9e8c21ece62675dd47b31
electrovoyage <75689188+electrovoyage@users.noreply.github.com>
2026-01-05 17:30:07 +0300

    correct installation path

--- cpm.lua
+++ cpm.lua
@@ -13,7 +13,7 @@ HANDLERS = {
         local tarball = http.get(releasedata.tarball_url):readAll()
         shell.run("mkdir /tmp")
         io.open(string.format("/tmp/%d.tar", releasedata.id), 'w'):write(tarball):close()
-        shell.run(string.format('tar -xf /tpm/%d.tar -C /cpm/%s', releasedata.id, package))
+        shell.run(string.format('tar -xf /tpm/%d.tar -C /cpm/packages/%s', releasedata.id, package))
         shell.run(string.format("rm /tmp/%d.tar", releasedata.id))
     end
 }
